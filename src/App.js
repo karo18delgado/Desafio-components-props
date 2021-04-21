44fdc6fc08bbafdf9b2339375de2f97b9afc9e2f
Carolina <karo18delgado@gmail.com>
2021-04-21 14:55:17 -0300

    Se agrego la Nav, Cards y Carrousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card';
+import NavB from './components/NavB';
+import Carrusel from './components/Carrusel';
 
 function App() {
+  const articulos = [
+    { image: "http://www.cinesunstar.com/images/2021/04/14/mortalkombat_poster.jpg", titulo: 'Mortal Kombat', descripcion: 'Acción,Aventuras y Fantasía' },
+    { image: "http://www.cinesunstar.com/images/2021/04/14/enguerraabuelo_poster.jpg", titulo: 'En Guerra con mi Abuelo', descripcion: 'Comedia y Familiar' },
+    { image: "http://www.cinesunstar.com/images/2021/04/06/pequenossecretos_poster.jpg", titulo: 'Pequeños Secretos', descripcion: 'Crimen y thriller' },
+    { image: "http://www.cinesunstar.com/images/2021/04/06/freaky_poster.jpg", titulo: 'Freaky', descripcion: 'Comedia y Terror' },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavB />
+      <Carrusel />
+      <div clasName="container">
+        <div className="row py-5">
+          {articulos.map((articulo) => (
+            <Card
+              articulo={articulo}
+            // imagen={articulo.image} 
+            // titulo={articulo.titulo} 
+            // descripcion={articulo.descripcion} 
+            />))}
+        </div>
+      </div>
     </div>
   );
 }
