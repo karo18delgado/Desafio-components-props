d1c1440bcfc467d7a037f3658c043edcfcc786fe
Carolina <karo18delgado@gmail.com>
2021-04-21 18:27:41 -0300

    se agrego props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,9 +10,9 @@ export default function Card(props) {
                         className="card-img-top imagen-card"
                         alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{props.articulo.titulo}</h5>
-                        <p className="card-text">{props.articulo.descripcion}</p>
-                        <p className="card-text">Puntaje: <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i></p>
+                        <h5 className="card-title titulo">{props.articulo.titulo}</h5>
+                        <p className="card-text texto">{props.articulo.descripcion}</p>
+                        <p className="card-text texto">Puntaje: <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i></p>
                         <a href="#" className="btn btn-dark">Ver</a>
                     </div>
                 </div>
