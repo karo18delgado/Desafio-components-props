9637999584bd7f47252a90b889446a17e0e30860
Carolina <karo18delgado@gmail.com>
2021-04-21 18:27:03 -0300

    agregue footer + setSection

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/Card';
 import NavB from './components/NavB';
 import Carrusel from './components/Carrusel';
+import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [section, setSection] = useState('Carrousel de estrenos')
+
   const articulos = [
     { image: "http://www.cinesunstar.com/images/2021/04/14/mortalkombat_poster.jpg", titulo: 'Mortal Kombat', descripcion: 'Acción,Aventuras y Fantasía' },
     { image: "http://www.cinesunstar.com/images/2021/04/14/enguerraabuelo_poster.jpg", titulo: 'En Guerra con mi Abuelo', descripcion: 'Comedia y Familiar' },
@@ -13,9 +17,11 @@ function App() {
   ]
   return (
     <div>
-      <NavB />
-      <Carrusel />
-      <div clasName="container">
+      <NavB setSection={setSection} />
+
+      {section === 'Carrousel de estrenos' && <Carrusel />}
+
+      {section=== 'Películas' && (<div >
         <div className="row py-5">
           {articulos.map((articulo) => (
             <Card
@@ -25,7 +31,10 @@ function App() {
             // descripcion={articulo.descripcion} 
             />))}
         </div>
-      </div>
+      </div>)}
+
+
+      <Footer />
     </div>
   );
 }
